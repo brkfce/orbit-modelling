449ff0f34f60dbfbeab947e7e90b11a2190353ed
brkfce <calsutton12@gmail.com>
2020-04-30 15:36:40 +0100

    Although with some limitations, this script will generate two bodies and calculate the velocity change necessary to intercept the other, and the relative angle between the bodies when the time is right to do so.

--- intercept.py
+++ intercept.py
@@ -1,4 +1,4 @@
-# this script will calculate the change in velocity required to change orbit 
+# this script will calculate the change in velocity required to change orbit
 # such that it will intercept with a target body, and the second velocity change
 # required to match orbit
 # assuming the two bodies are in stable circular orbits around a third body
@@ -14,7 +14,7 @@ class point:
     def __init__(self, r, t):
         self.r = r
         self.t = t
-        
+
 class body:
     def __init__(self, position, initRadius, mass, velocity, designation):
         self.position = position
@@ -45,18 +45,37 @@ def createOrbits(radius1, radius2, gravConstant, centralMass):
 
     return bodiesList
 
+# given the orbital velocites calculated when constructing the bodies, this
+# computes the change of velocity necessary to make the orbits intercept
+# the change in kinetic energy of the body on one side of the orbit will
+# change the potential energy of the body on the other side of the orbit
+# causing a corresponding change in orbital radius
+# note that this assumes that the change in velocity is instant, which is unrealistic
+# future versions will account for limited thrust capabilities
 def calcInterceptVelocity(bodiesList, gravConstant):
-
-    # given the orbital velocites calculated when constructing the bodies, this
-    # computes the change of velocity necessary to make the orbits intercept
-    # the change in kinetic energy of the body on one side of the orbit will
-    # change the potential energy of the body on the other side of the orbit
-    # causing a corresponding change in orbital radius
-
     radiusChange = bodiesList[1].initRadius - bodiesList[2].initRadius
     potentialChange = ( gravConstant * bodiesList[0].mass * bodiesList[2].mass ) / radiusChange
     vChange = math.sqrt( 2 * potentialChange / bodiesList[2].mass )
     return vChange
+
+# calculate the time it will take for the interceptor to complete half of its new
+# orbit, when it will intercept the orbit of the other object
+def calcEllipsePeriod(radius1, radius2, gravConstant, centralMass):
+    semiMajor = (radius1 + radius2) / 2
+    pSquared = (4 * 3.14**2)(semiMajor)**3 / (gravConstant * centralMass)
+    period = math.sqrt(pSquared)
+    return period
+
+# the stable object must be interceptTime * velocity away from the intercept
+# point at the time of the velocity change. Hence, the relative angle between
+# the objects must be the difference between pi (the angle covered by the
+# interceptor to intercept) and the angle swept out by the stable object in
+# time interceptTime
+def relativeAngle(interceptTime, bodiesData):
+    interceptorSweepAngle = 3.14
+    stableSweepAngle = interceptTime * bodiesData[1].velocity.t
+    relative = interceptorSweepAngle - stableSweepAngle
+
 
 if __name__ == '__main__':
 
@@ -65,8 +84,13 @@ if __name__ == '__main__':
     radius1 = 1000
     radius2 = 100000
     centralMass = 1e6
-    
-    bodiesData = createOrbits(radius1, radius2, gravConstant, centralMass)
-    velocityChange = calcInterceptVelocity(bodiesData, gravConstant) 
 
-    
+    bodiesData = createOrbits(radius1, radius2, gravConstant, centralMass)
+    velocityChange = calcInterceptVelocity(bodiesData, gravConstant)
+    interceptTime = calcEllipsePeriod(radius1, radius2, gravConstant, centralMass) / 2
+    # note that the relative angle is measured from the interceptor to the stable object
+    # at the correct time to change the velocity of the interceptor
+    angle = relativeAngle(interceptTime, bodiesData)
+
+    print("Change in velocity required: " + velocityChange)
+    print("Relative angle at burn time: " + angle)
